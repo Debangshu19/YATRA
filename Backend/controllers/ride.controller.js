61d2e7c2477d4bf006e4b7ae9276059b6a0e5602
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2024-12-21 12:17:34 +0530

    Update ride.controller.js

--- Backend/controllers/ride.controller.js
+++ Backend/controllers/ride.controller.js
@@ -64,7 +64,7 @@ module.exports.confirmRide = async (req, res) => {
     const { rideId } = req.body;
 
     try {
-        const ride = await rideService.confirmRide({ rideId, captain: req.captain });
+        const ride = await rideService.confirmRide({ rideId, captain: { _id: req.captain._id } });
 
         sendMessageToSocketId(ride.user.socketId, {
             event: 'ride-confirmed',
