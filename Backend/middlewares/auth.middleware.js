0f189abc72d3bad1a0bcef238312c188d533d90b
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2024-12-21 11:49:26 +0530

    Update auth.middleware.js

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -31,8 +31,7 @@ module.exports.authUser = async (req, res, next) => {
 }
 
 module.exports.authCaptain = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
-
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' });
@@ -40,21 +39,22 @@ module.exports.authCaptain = async (req, res, next) => {
 
     const isBlacklisted = await BlacklistTokenModel.findOne({ token: token });
 
-
-
     if (isBlacklisted) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const captain = await captainModel.findById(decoded._id)
-        req.captain = captain;
+        const captain = await captainModel.findById(decoded._id);
+        
+        if (!captain) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
 
-        return next()
+        req.captain = captain;
+        return next();
     } catch (err) {
         console.log(err);
-
-        res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: 'Unauthorized' });
     }
 }
